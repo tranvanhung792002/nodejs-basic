48ead870cb0187dacbdf3e3fc470ce2cc122fd9a
Hung <tranvanhung792002@gmail.com>
2024-09-19 15:58:34 +0700

    ngay3

--- src/server.js
+++ src/server.js
@@ -1,15 +1,17 @@
-const express = require('express');
+import express from 'express';
+import configViewEngine from './configs/viewEngine';
 
 const app = express();
 const port = 8080
 
+configViewEngine(app);
+
 app.get('/', (req, res) => {
-    res.send('Hello World! vs Hoi Dan IT and Eric')
+    res.render('index.ejs')
 })
 app.get('/about', (req, res) => {
     res.send(`I'm Eric!`)
 })
-
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
