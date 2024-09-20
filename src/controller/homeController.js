d48e53ac436da66a653831adec22ea912743eb33
Hung <tranvanhung792002@gmail.com>
2024-09-20 13:22:59 +0700

    ngay4-v1

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,8 +1,26 @@
-
+import connection from '../configs/connectDB';
 let getHomepage = (req, res) => {
-    return res.render('index.ejs')
-}
+    //logic
+    let data = [];
+    connection.query(
+        'SELECT * FROM `users` ',
+        function (err, results, fields) {
+            results.map((row) => {
+                data.push({
+                    id: row.id,
+                    email: row.email,
+                    address: row.address,
+                    firstname: row.firstname,
+                    name: row.name
+                })
 
+            });
+
+            return res.render('index.ejs', { dataUser: data, test: 'abc string test' })
+
+        })
+
+}
 module.exports = {
     getHomepage
 }
